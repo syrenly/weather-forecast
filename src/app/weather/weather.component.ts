7a3dac53e7a1e0055fe44ea24f1f71377b83ce15
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-13 09:48:02 +0200

    added components

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.scss'
+})
+export class WeatherComponent {
+
+}
