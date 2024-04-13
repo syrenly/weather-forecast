b12b2c1834999b20869d8f4484d236f2bcb7ab08
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-13 09:48:14 +0200

    added routing and animations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,22 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
+import { WeatherComponent } from "./weather/weather.component";
 
 export const routes: Routes = [
 	{
 		path: "",
-		redirectTo: "home",
-	},
-	{
-		path: "home",
-		component: HomeComponent,
+		children: [
+			{
+				path: "home",
+				component: HomeComponent,
+				data: { animationState: "homeState" },
+			},
+			{
+				path: "weather",
+				component: WeatherComponent,
+				data: { animationState: "weatherState" },
+			},
+		],
 	},
 	{
 		path: "**",
