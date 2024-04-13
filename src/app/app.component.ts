a501a9f99768008ae20b62f79abb11fc97c08b36
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-13 09:48:14 +0200

    added routing and animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { MatIconRegistry } from "@angular/material/icon";
+import { RouterLink, RouterOutlet } from "@angular/router";
+import { routeTransitionAnimations } from "./route-transition-animations";
 
 @Component({
 	selector: "app-root",
 	standalone: true,
-	imports: [RouterOutlet],
+	imports: [RouterOutlet, RouterLink],
 	templateUrl: "./app.component.html",
 	styleUrl: "./app.component.scss",
+	animations: [routeTransitionAnimations],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 	title = "weather-forecast";
+	constructor(private matIconRegistry: MatIconRegistry) {}
+
+	ngOnInit(): void {
+		this.matIconRegistry.setDefaultFontSetClass(
+			"material-symbols-outlined"
+		);
+	}
+	prepareRoute(outlet: RouterOutlet) {
+		return (
+			outlet &&
+			outlet.activatedRouteData &&
+			outlet.activatedRouteData["animationState"]
+		);
+	}
 }
