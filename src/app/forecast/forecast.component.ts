4f3b5041760f0fdc6c435c82cfa5978e8c0ebc0f
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-13 19:32:51 +0200

    added card

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,31 +1,49 @@
+import { NgOptimizedImage } from "@angular/common";
 import { Component, DestroyRef, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { MatDialog } from "@angular/material/dialog";
+import { MatCardModule } from "@angular/material/card";
 import { MatIconModule } from "@angular/material/icon";
 import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { FlagPipe } from "../pipes/flag.pipe";
+import { WeatherPipe } from "../pipes/weather.pipe";
+import { ICity, IWeather } from "../types";
 import { SearchbarComponent } from "../ui-components/searchbar/searchbar.component";
+import { SearchService } from "./../services/search.service";
 
 @Component({
 	selector: "app-forecast",
 	standalone: true,
-	imports: [SearchbarComponent, RouterLink, MatIconModule],
+	imports: [
+		SearchbarComponent,
+		RouterLink,
+		MatIconModule,
+		MatCardModule,
+		NgOptimizedImage,
+		FlagPipe,
+		WeatherPipe,
+	],
 	templateUrl: "./forecast.component.html",
 	styleUrl: "./forecast.component.scss",
 })
 export class ForecastComponent implements OnInit {
+	city: ICity | undefined;
+	get mainWeather(): IWeather | undefined {
+		return this.city?.weather ? this.city.weather[0] : undefined;
+	}
+
 	constructor(
 		private readonly route: ActivatedRoute,
 		private readonly router: Router,
-		private readonly destroyRef: DestroyRef,
-		public dialog: MatDialog
+		private readonly searchService: SearchService,
+		private readonly destroyRef: DestroyRef
 	) {}
 	ngOnInit(): void {
 		this.route.data
 			.pipe(takeUntilDestroyed(this.destroyRef))
 			.subscribe((value) => {
 				console.log(value);
+				this.city = value[0] as ICity;
+				// this.searchService.
 			});
 	}
-
-	openSearchDialog(): void {}
 }
