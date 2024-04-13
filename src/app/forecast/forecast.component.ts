e6f025ee7d5485f2eef558f1ef9de5bb64dd71be
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-13 18:56:06 +0200

    fixed forecast

--- src/app/forecast/forecast.component.ts
+++ src/app/forecast/forecast.component.ts
@@ -1,11 +1,14 @@
 import { Component, DestroyRef, OnInit } from "@angular/core";
 import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
-import { ActivatedRoute, Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
+import { MatIconModule } from "@angular/material/icon";
+import { ActivatedRoute, Router, RouterLink } from "@angular/router";
+import { SearchbarComponent } from "../ui-components/searchbar/searchbar.component";
 
 @Component({
 	selector: "app-forecast",
 	standalone: true,
-	imports: [],
+	imports: [SearchbarComponent, RouterLink, MatIconModule],
 	templateUrl: "./forecast.component.html",
 	styleUrl: "./forecast.component.scss",
 })
@@ -13,7 +16,8 @@ export class ForecastComponent implements OnInit {
 	constructor(
 		private readonly route: ActivatedRoute,
 		private readonly router: Router,
-		private readonly destroyRef: DestroyRef
+		private readonly destroyRef: DestroyRef,
+		public dialog: MatDialog
 	) {}
 	ngOnInit(): void {
 		this.route.data
@@ -22,4 +26,6 @@ export class ForecastComponent implements OnInit {
 				console.log(value);
 			});
 	}
+
+	openSearchDialog(): void {}
 }
