3f94e687f39cbc35942104632e67feff3de50657
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-14 21:24:16 +0200

    added comments

--- src/app/pipes/flag.pipe.ts
+++ src/app/pipes/flag.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from "@angular/core";
-
+/**
+ * Retrieve the right url for the image of the flag using the code of the country (lowercase)
+ */
 @Pipe({
 	name: "flag",
 	standalone: true,
 })
 export class FlagPipe implements PipeTransform {
 	transform(country: string | undefined): string {
+		if (!country) {
+			return "";
+		}
 		const lowerCaseCountry = country.toLowerCase();
 		return `http://openweathermap.org/images/flags/${lowerCaseCountry}.png`;
 	}
--- src/app/pipes/weather.pipe.ts
+++ src/app/pipes/weather.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from "@angular/core";
-
+/**
+ * Retrieve the right url for the image of the weather conditions using a specific code
+ */
 @Pipe({
 	name: "weather",
 	standalone: true,
 })
 export class WeatherPipe implements PipeTransform {
-	transform(icon: string | undefined): string {
-		return `http://openweathermap.org/img/w/${icon}.png`;
+	transform(weatherIconCode: string | undefined): string {
+		if (!weatherIconCode) {
+			return "";
+		}
+		return `http://openweathermap.org/img/w/${weatherIconCode}.png`;
 	}
 }
