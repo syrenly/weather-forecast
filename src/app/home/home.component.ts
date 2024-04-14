d93736614e65e3b1b6c90f99c0a9e99edc5eef1e
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-14 20:45:22 +0200

    fixed homepage

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,22 +4,30 @@ import { MatCardModule } from "@angular/material/card";
 import { Router } from "@angular/router";
 import { SearchbarComponent } from "../ui-components/searchbar/searchbar.component";
 import { SwitchThemeComponent } from "../ui-components/switch-theme/switch-theme.component";
-
+/**
+ * The HomeComponent is the initial page for the application.
+ * - toggle button to switch between light and dark theme
+ * - title
+ * - small description
+ * - search bar to choose a city to retrieve its weather forecasts
+ * - buttons with specific city ids
+ */
 @Component({
 	selector: "app-home",
 	standalone: true,
 	imports: [
-		SearchbarComponent,
 		MatCardModule,
-		SwitchThemeComponent,
 		MatButtonModule,
+		SearchbarComponent,
+		SwitchThemeComponent,
 	],
 	templateUrl: "./home.component.html",
 	styleUrl: "./home.component.scss",
 })
 export class HomeComponent {
 	constructor(private readonly router: Router) {}
-	navigateTo(id: number): void {
-		this.router.navigate(["forecast", id]);
+
+	navigateTo(cityId: number): void {
+		this.router.navigate(["forecast", cityId]);
 	}
 }
