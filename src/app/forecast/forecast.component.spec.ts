ba7278a415b969a7f2007f3bae9be38122c9d530
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-14 20:50:03 +0200

    added comments for forecast

--- src/app/forecast/forecast.component.spec.ts
+++ src/app/forecast/forecast.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ForecastComponent } from './forecast.component';
+import { ForecastComponent } from "./forecast.component";
 
-describe('ForecastComponent', () => {
-  let component: ForecastComponent;
-  let fixture: ComponentFixture<ForecastComponent>;
+describe("ForecastComponent", () => {
+	let component: ForecastComponent;
+	let fixture: ComponentFixture<ForecastComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ForecastComponent]
-    })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(ForecastComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [ForecastComponent],
+		}).compileComponents();
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+		fixture = TestBed.createComponent(ForecastComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it("should create", () => {
+		expect(component).toBeTruthy();
+	});
 });
