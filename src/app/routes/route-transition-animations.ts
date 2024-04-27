dbb74af75d87e60052504583e23954215398bf3a
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-27 09:32:55 +0200

    applied reverse transition to navigation

--- src/app/routes/route-transition-animations.ts
+++ src/app/routes/route-transition-animations.ts
@@ -1,7 +1,7 @@
 import { animate, animateChild, group, query, style, transition, trigger } from "@angular/animations";
 
 export const routeTransitionAnimations = trigger("animateRoute", [
-	transition("homeState => forecastState, forecastState => homeState", [
+	transition("homeState => forecastState", [
 		style({ position: "relative" }),
 		query(":enter, :leave", [
 			style({
@@ -16,6 +16,24 @@ export const routeTransitionAnimations = trigger("animateRoute", [
 		group([
 			query(":leave", [animate("1s ease-out", style({ left: "-100%", opacity: 0 }))]),
 			query(":enter", [animate("1s ease-out", style({ left: "0%", opacity: 1 }))]),
+		]),
+		query(":enter", animateChild()),
+	]),
+	transition("forecastState => homeState", [
+		style({ position: "relative" }),
+		query(":enter, :leave", [
+			style({
+				position: "absolute",
+				top: 0,
+				right: 0,
+				width: "100%",
+			}),
+		]),
+		query(":enter", [style({ right: "100%", opacity: 0 })]),
+		query(":leave", animateChild()),
+		group([
+			query(":leave", [animate("1s ease-out", style({ right: "-100%", opacity: 0 }))]),
+			query(":enter", [animate("1s ease-out", style({ right: "0%", opacity: 1 }))]),
 		]),
 		query(":enter", animateChild()),
 	]),
