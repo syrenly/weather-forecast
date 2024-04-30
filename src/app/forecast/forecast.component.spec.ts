3ac6b8f107af6db2ab113f1c9dce164b2997a1c0
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-30 22:06:30 +0200

    added test to statements of forecast component

--- src/app/forecast/forecast.component.spec.ts
+++ src/app/forecast/forecast.component.spec.ts
@@ -1,28 +1,82 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed, fakeAsync, tick } from "@angular/core/testing";
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import { ActivatedRoute } from "@angular/router";
-import { of } from "rxjs";
+import { ActivatedRoute, Router } from "@angular/router";
+import { BehaviorSubject } from "rxjs";
 import { getSearchMockProvider } from "../unit-test-utils/search.service.mock";
 import { provideMockTheme } from "../unit-test-utils/token.mock";
 import { city, forecastResult } from "../unit-test-utils/utils.mock";
 import { ForecastComponent } from "./forecast.component";
 
 const route = {
-	data: of({
-		countryInfo: city,
-		forecastResult: forecastResult,
-		animationState: "forecastState",
+	data: new BehaviorSubject({
+		0: {
+			countryInfo: city,
+			forecastResult: forecastResult,
+			animationState: "forecastState",
+		},
 	}),
 };
 
-describe("ForecastComponent", (): void => {
+describe("ForecastComponent case success route", (): void => {
+	let component: ForecastComponent;
+	let router: Router;
+	let fixture: ComponentFixture<ForecastComponent>;
+
+	beforeEach(async (): Promise<void> => {
+		await TestBed.configureTestingModule({
+			imports: [ForecastComponent, NoopAnimationsModule],
+			providers: [
+				getSearchMockProvider(),
+				provideMockTheme(),
+				{ provide: ActivatedRoute, useValue: route },
+				Router,
+			],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(ForecastComponent);
+		router = TestBed.inject(Router);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
+	});
+
+	it("should create", (): void => {
+		expect(component).toBeTruthy();
+	});
+	it("should set data from route", fakeAsync((): void => {
+		tick(10);
+		expect(component.city).toEqual(city);
+		expect(component.forecastResult).toEqual(forecastResult);
+		expect(component.errorStatus).toBeUndefined();
+	}));
+	it("should navigate to selected city", (): void => {
+		const routerSpy = spyOn(router, "navigateByUrl");
+		component.navigateToCity(city);
+		expect(routerSpy).toHaveBeenCalledWith(`/forecast/${city.id}`, {
+			state: city,
+		});
+	});
+});
+
+const errorInRoute = {
+	data: new BehaviorSubject({
+		0: {
+			errorStatus: 404,
+			animationState: "forecastState",
+		},
+	}),
+};
+describe("ForecastComponent case error status", (): void => {
 	let component: ForecastComponent;
 	let fixture: ComponentFixture<ForecastComponent>;
 
 	beforeEach(async (): Promise<void> => {
 		await TestBed.configureTestingModule({
 			imports: [ForecastComponent, NoopAnimationsModule],
-			providers: [getSearchMockProvider(), provideMockTheme(), { provide: ActivatedRoute, useValue: route }],
+			providers: [
+				getSearchMockProvider(),
+				provideMockTheme(),
+				{ provide: ActivatedRoute, useValue: errorInRoute },
+			],
 		}).compileComponents();
 
 		fixture = TestBed.createComponent(ForecastComponent);
@@ -33,4 +87,10 @@ describe("ForecastComponent", (): void => {
 	it("should create", (): void => {
 		expect(component).toBeTruthy();
 	});
+	it("should set error from route data", fakeAsync((): void => {
+		tick(10);
+		expect(component.city).toBeUndefined();
+		expect(component.forecastResult).toBeUndefined();
+		expect(component.errorStatus).toBe(404);
+	}));
 });
