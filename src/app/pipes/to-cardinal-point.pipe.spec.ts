89eae1c3addd5da71f50f2a324b4df8178dc45c9
syrenly <150293005+syrenly@users.noreply.github.com>
2024-04-30 22:31:25 +0200

    added tests to statements of the pipes

--- src/app/pipes/to-cardinal-point.pipe.spec.ts
+++ src/app/pipes/to-cardinal-point.pipe.spec.ts
@@ -1,8 +1,56 @@
-import { ToCardinalPointPipe } from './to-cardinal-point.pipe';
+import { Component } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { ToCardinalPointPipe } from "./to-cardinal-point.pipe";
 
-describe('ToCardinalPointPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ToCardinalPointPipe();
-    expect(pipe).toBeTruthy();
-  });
+@Component({
+	standalone: true,
+	imports: [ToCardinalPointPipe],
+	template: `
+		<div class="wind-class">{{ windDirectionDegree | toCardinalPoint }}</div>
+	`,
+})
+class TestComponent {
+	windDirectionDegree: number | undefined;
+}
+
+describe("ToCardinalPointPipe", (): void => {
+	describe("create the object", (): void => {
+		it("should create an instance", (): void => {
+			const pipe = new ToCardinalPointPipe();
+			expect(pipe).toBeTruthy();
+		});
+		it("should return empty string from an undefined number of degrees", (): void => {
+			const pipe = new ToCardinalPointPipe();
+			expect(pipe.transform(undefined)).toBe("");
+		});
+		it("should return wind direction from a defined number of degrees", (): void => {
+			const pipe = new ToCardinalPointPipe();
+			expect(pipe.transform(180)).toBe("S");
+		});
+	});
+	describe("html pipe", (): void => {
+		let component: TestComponent;
+		let fixture: ComponentFixture<TestComponent>;
+
+		beforeEach(async (): Promise<void> => {
+			await TestBed.configureTestingModule({
+				imports: [TestComponent],
+			}).compileComponents();
+
+			fixture = TestBed.createComponent(TestComponent);
+			component = fixture.componentInstance;
+			fixture.detectChanges();
+		});
+		it("should return empty string from undefined degrees", (): void => {
+			const div = fixture.debugElement.query(By.css(".wind-class"));
+			expect(div.nativeElement.innerHTML).toBe("");
+		});
+		it("should return wind direction from defined degrees", (): void => {
+			component.windDirectionDegree = 180;
+			fixture.detectChanges();
+			const div = fixture.debugElement.query(By.css(".wind-class"));
+			expect(div.nativeElement.innerHTML).toBe("S");
+		});
+	});
 });
--- src/app/pipes/weather.pipe.spec.ts
+++ src/app/pipes/weather.pipe.spec.ts
@@ -1,8 +1,62 @@
+import { Component } from "@angular/core";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
 import { WeatherPipe } from "./weather.pipe";
 
+@Component({
+	standalone: true,
+	imports: [WeatherPipe],
+	template: `
+		<div class="weather-class">{{ weatherIconCode | weather }}</div>
+	`,
+})
+class TestComponent {
+	weatherIconCode: string | undefined;
+}
+
 describe("WeatherPipe", (): void => {
-	it("create an instance", (): void => {
-		const pipe = new WeatherPipe();
-		expect(pipe).toBeTruthy();
+	describe("create the object", (): void => {
+		it("should create an instance", (): void => {
+			const pipe = new WeatherPipe();
+			expect(pipe).toBeTruthy();
+		});
+		it("should return empty string from undefined icon", (): void => {
+			const pipe = new WeatherPipe();
+			expect(pipe.transform(undefined)).toBe("");
+		});
+		it("should return image url from a string", (): void => {
+			const pipe = new WeatherPipe();
+			expect(pipe.transform("clouds")).toBe("http://openweathermap.org/img/w/clouds.png");
+		});
+	});
+	describe("html pipe", (): void => {
+		let component: TestComponent;
+		let fixture: ComponentFixture<TestComponent>;
+
+		beforeEach(async (): Promise<void> => {
+			await TestBed.configureTestingModule({
+				imports: [TestComponent],
+			}).compileComponents();
+
+			fixture = TestBed.createComponent(TestComponent);
+			component = fixture.componentInstance;
+			fixture.detectChanges();
+		});
+		it("should return empty url from a undefined icon", (): void => {
+			const div = fixture.debugElement.query(By.css(".weather-class"));
+			expect(div.nativeElement.innerHTML).toBe("");
+		});
+		it("should return an empty string from an empty string", (): void => {
+			component.weatherIconCode = "";
+			fixture.detectChanges();
+			const div = fixture.debugElement.query(By.css(".weather-class"));
+			expect(div.nativeElement.innerHTML).toBe("");
+		});
+		it("should return image url from a defined icon", (): void => {
+			component.weatherIconCode = "clouds";
+			fixture.detectChanges();
+			const div = fixture.debugElement.query(By.css(".weather-class"));
+			expect(div.nativeElement.innerHTML).toBe("http://openweathermap.org/img/w/clouds.png");
+		});
 	});
 });
