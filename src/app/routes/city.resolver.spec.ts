81e3203491a2aa50a393cceef3518b9ec152e14d
syrenly <150293005+syrenly@users.noreply.github.com>
2024-05-01 12:37:03 +0200

    updated tests for resolver and guard

--- src/app/routes/city.resolver.spec.ts
+++ src/app/routes/city.resolver.spec.ts
@@ -1,9 +1,15 @@
-import { TestBed } from "@angular/core/testing";
-import { MaybeAsync, ResolveFn } from "@angular/router";
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
+import { ActivatedRouteSnapshot, MaybeAsync, ResolveFn, Router, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
+import { SearchService } from "../services/search.service";
+import { ICityWeather } from "../types/city-types";
+import { IFiveDaysForecast } from "../types/forecast-types";
+import { getSearchMockProvider } from "../unit-test-utils/search.service.mock";
+import { city, forecastResult } from "../unit-test-utils/utils.mock";
 import { CityResolverType, cityResolver } from "./city.resolver";
 
 describe("cityResolver", (): void => {
+	let searchService: SearchService;
 	const executeResolver: ResolveFn<Observable<CityResolverType>> = (
 		...resolverParameters
 	): MaybeAsync<Observable<CityResolverType>> =>
@@ -12,10 +18,35 @@ describe("cityResolver", (): void => {
 		);
 
 	beforeEach((): void => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			providers: [getSearchMockProvider(), Router],
+		});
+		searchService = TestBed.inject(SearchService);
 	});
 
 	it("should be created", (): void => {
 		expect(executeResolver).toBeTruthy();
 	});
+
+	it("should have success", fakeAsync((): void => {
+		const route = new ActivatedRouteSnapshot();
+		route.params = { id: 1 };
+		(executeResolver(route, {} as RouterStateSnapshot) as Observable<CityResolverType>).subscribe(value => {
+			const v = value as { countryInfo: ICityWeather; forecastResult: IFiveDaysForecast };
+			expect(v.countryInfo).toEqual(city);
+			expect(v.forecastResult).toEqual(forecastResult);
+			expect(searchService.navigationStarted).toBeTrue();
+		});
+		tick();
+	}));
+
+	it("should handle error 404", fakeAsync((): void => {
+		const route = new ActivatedRouteSnapshot();
+		(executeResolver(route, {} as RouterStateSnapshot) as Observable<CityResolverType>).subscribe(value => {
+			const v = value as { errorStatus: number };
+			expect(v.errorStatus).toBe(404);
+			expect(searchService.navigationStarted).toBeFalsy();
+		});
+		tick();
+	}));
 });
--- src/app/routes/forecast-details.guard.spec.ts
+++ src/app/routes/forecast-details.guard.spec.ts
@@ -1,16 +1,63 @@
-import { TestBed } from "@angular/core/testing";
-import { CanActivateFn, GuardResult, MaybeAsync } from "@angular/router";
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
+import {
+	ActivatedRouteSnapshot,
+	CanActivateFn,
+	GuardResult,
+	MaybeAsync,
+	Router,
+	RouterStateSnapshot,
+} from "@angular/router";
+import { Observable } from "rxjs";
+import { provideMockEMPTYWeatherApiKey, provideMockWeatherApiKey } from "../unit-test-utils/token.mock";
 import { forecastDetailsGuard } from "./forecast-details.guard";
 
-describe("forecastDetailsGuard", (): void => {
+describe("forecastDetailsGuard - case positive", (): void => {
 	const executeGuard: CanActivateFn = (...guardParameters): MaybeAsync<GuardResult> =>
 		TestBed.runInInjectionContext((): MaybeAsync<GuardResult> => forecastDetailsGuard(...guardParameters));
 
 	beforeEach((): void => {
-		TestBed.configureTestingModule({});
+		TestBed.configureTestingModule({
+			providers: [provideMockWeatherApiKey(), Router],
+		});
 	});
 
 	it("should be created", (): void => {
 		expect(executeGuard).toBeTruthy();
 	});
+	it("should to let proceed", fakeAsync((): void => {
+		(executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot) as Observable<GuardResult>).subscribe(
+			value => {
+				expect(value).toBeTrue();
+			}
+		);
+		tick();
+	}));
+});
+
+describe("forecastDetailsGuard - case negative", (): void => {
+	let router: Router;
+	const executeGuard: CanActivateFn = (...guardParameters): MaybeAsync<GuardResult> =>
+		TestBed.runInInjectionContext((): MaybeAsync<GuardResult> => forecastDetailsGuard(...guardParameters));
+
+	beforeEach((): void => {
+		TestBed.configureTestingModule({
+			providers: [provideMockEMPTYWeatherApiKey(), Router],
+		});
+		router = TestBed.inject(Router);
+	});
+
+	it("should be created", (): void => {
+		expect(executeGuard).toBeTruthy();
+	});
+	it("should not to let proceed", fakeAsync((done: DoneFn): void => {
+		const routerSpy = spyOn(router, "navigate");
+		(executeGuard({} as ActivatedRouteSnapshot, {} as RouterStateSnapshot) as Observable<GuardResult>).subscribe(
+			value => {
+				expect(value).toBeFalse();
+				expect(routerSpy).toHaveBeenCalledWith(["home"]);
+				done();
+			}
+		);
+		tick();
+	}));
 });
