01c09f74562f5aa2803c7a7f469932179a2078a8
syrenly <150293005+syrenly@users.noreply.github.com>
2024-05-18 00:14:36 +0200

    used Renderer2 to manipulate DOM

--- src/app/consts.ts
+++ src/app/consts.ts
@@ -1,3 +1,4 @@
+import { Theme } from "./tokens";
 import { ICitySearchResult } from "./types/city-types";
 
 export const DEFAULT_DEBOUNCE_DELAY_MILLISECONDS = 250;
@@ -8,3 +9,5 @@ export const EMPTY_SEARCH_RESULT: ICitySearchResult = {
 	count: 0,
 	list: [],
 };
+// Arrow function to retrieve the css class to apply a theme
+export const themeCssClass = (themeName: Theme): string => `${themeName}-theme`;
