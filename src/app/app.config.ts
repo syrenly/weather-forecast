cacb20cf544966e5319a533164ad443aa3750879
syrenly <150293005+syrenly@users.noreply.github.com>
2024-11-15 23:01:44 +0100

    consolidating ng 18 version

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideAnimations } from "@angular/platform-browser/animations";
 import { provideRouter } from "@angular/router";
 import { routes } from "./routes/app.routes";
 import { WEATHER_API_KEY, initializeApp, provideCurrentTheme, provideWeatherApiKey } from "./tokens";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -26,6 +27,6 @@ export const appConfig: ApplicationConfig = {
 		{
 			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
 			useValue: { appearance: "outline" },
-		},
+		}, provideAnimationsAsync(),
 	],
 };
