1ad3cb6e5d82707b82780eab83e655633511bcb3
syrenly <150293005+syrenly@users.noreply.github.com>
2025-04-05 17:04:50 +0200

    update to ng19 - part7

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default defineConfig([globalIgnores([
+    "**/.nx/",
+    ".vscode/*",
+    "**/*.yaml",
+    "**/*.yml",
+    "**/*.mock.ts",
+    "**/coverage/",
+    "dist/*",
+    "**/Docker",
+    "node_modules/*",
+]), {
+    files: ["**/*.ts"],
+
+    extends: compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@angular-eslint/recommended",
+        "plugin:@angular-eslint/template/process-inline-templates",
+        "plugin:prettier/recommended",
+    ),
+
+    rules: {
+        "@angular-eslint/directive-selector": ["error", {
+            type: "attribute",
+            prefix: "app",
+            style: "camelCase",
+        }],
+
+        "@angular-eslint/component-selector": ["error", {
+            type: "element",
+            prefix: "app",
+            style: "kebab-case",
+        }],
+
+        "prettier/prettier": "error",
+    },
+}, {
+    files: ["**/*.html"],
+
+    extends: compat.extends(
+        "plugin:@angular-eslint/template/recommended",
+        "plugin:@angular-eslint/template/accessibility",
+    ),
+
+    rules: {},
+}]);
