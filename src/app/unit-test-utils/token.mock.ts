82bc9257c7239dce04685a0b567d12b4b44b6527
syrenly <150293005+syrenly@users.noreply.github.com>
2025-08-15 11:53:45 +0200

    [WIP] Fixed unit tests

--- src/app/unit-test-utils/token.mock.ts
+++ src/app/unit-test-utils/token.mock.ts
@@ -1,15 +1,20 @@
-import { IS_DEV_MODE } from "./../tokens";
 /**
  * File containing mocked providers to be used in unit tests
  */
 
 import { LOCALE_ID, Provider } from "@angular/core";
 import { BehaviorSubject } from "rxjs";
+import { DUMMY_API_KEY } from "../consts";
 import { CURRENT_THEME, WEATHER_API_KEY } from "../tokens";
 
 export const provideMockWeatherApiKey = (): Provider => ({
 	provide: WEATHER_API_KEY,
 	useValue: new BehaviorSubject<string>("KEY"),
+});
+
+export const provideMockDummyWeatherApiKey = (): Provider => ({
+	provide: WEATHER_API_KEY,
+	useValue: new BehaviorSubject<string>(DUMMY_API_KEY),
 });
 
 export const provideMockTheme = (): Provider => ({
